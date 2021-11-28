fea2c6e8dfa1563b1174aa62adf67f253fd0e8f5
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-11-28 10:04:52 +0100

    feat: Request

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return {'Hello': 'World Flask!'}
+    user_ip = request.remote_addr
+    return {
+        'Hello': 'World Flask!',
+        'IP': user_ip
+    }
